b90e7a615f62acac017ae44e56fee06a3b4460b8
NicolasBroyad <113180571+NicolasBroyad@users.noreply.github.com>
2022-10-12 22:47:38 -0300

    .

--- Parador 5 (proyecto)/parador.js
+++ Parador 5 (proyecto)/parador.js
@@ -1,21 +1,8 @@
-function print (txt,end="<br>") {
-    document.body.innerHTML+=txt+end;
-}
-
-function input(cad){
-    var res= prompt(cad);
-    return res;
-}
-
-//hijo de re mil puta
-
-function operNum(a,b){
-    suma= a+b
-    resta= a-b
-    division= a/b 
-    producto= a*b 
-    potencia= a**b
-    resto= a%b
-    print("suma: "+ suma)
-    print("resta: "+ resta)
+function valEmail(valor){
+    re=/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,3})$/
+    if(!re.exec(valor))    {
+        return false;
+    }else{
+        return true;
+    }
 }
