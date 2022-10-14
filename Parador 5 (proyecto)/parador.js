f40f4a52a4f634db00ff284fe9bcdf5c1babb09b
mateodelafuente <113181038+mateodelafuente@users.noreply.github.com>
2022-10-14 09:59:49 -0300

    Update parador.js

--- Parador 5 (proyecto)/parador.js
+++ Parador 5 (proyecto)/parador.js
@@ -1,8 +1,36 @@
-function valEmail(valor){
-    re=/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,3})$/
-    if(!re.exec(valor))    {
-        return false;
-    }else{
-        return true;
+//Funcion de validar informacion para los formularios
+
+function validarFormulario() {
+    //VALIDACION NOMBRE
+    var mensaje = document.getElementById("error_nombre").value;
+    var nombre = document.getElementById('nombre').value;
+    if(nombre.length == 0) {
+        alert("La seccion del nombre no esta completa");
+    }
+
+    //VALIDACION APELLIDO
+    var apellido = document.getElementById('apellido').value;
+    if (apellido.length == 0) {
+        alert('El apellido no es válido');
+    }
+
+    //VALIDACION MAIL
+    var email = document.getElementById("email").value;
+    if(email.validity.valueMissing) {
+        // Si el campo está vacío
+        // muestra el mensaje de error siguiente.
+        alert("Falta completar la caja del mail");
+
+    } else if(email.validity.typeMismatch) {
+        // Si el campo no contiene una dirección de correo electrónico
+        // muestra el mensaje de error siguiente.
+        alert("La dirección de correo electrónico ingresada no es valida");
+    
+    }
+
+    //VALIDACION APELLIDO
+    var contraseña = document.getElementById('contraseña').value;
+    if (contraseña.length < 6) {
+        alert('La contraseña no es válida, al menos 6 caractéres tiene que tener');
     }
 }
