fe69fc358c08cf35aadfe501f76970149ccc7f7e
NicolasBroyad <113180571+NicolasBroyad@users.noreply.github.com>
2022-11-04 08:45:46 -0300

    .

---
+++ Parador 5 (proyecto)/validar_fixture.js
@@ -0,0 +1,181 @@
+function validarEquiposOctavos1() {
+    var combo = document.getElementById("equipo1_octavos1");
+    var equipo1 = combo.options[combo.selectedIndex].text;
+    var combo1 = document.getElementById("equipo2_octavos1");
+    var equipo2 = combo1.options[combo1.selectedIndex].text;
+
+    if (equipo1 == equipo2) {
+        alert("ERROR. No se puede enfrentar un mismo equipo entre si.");
+        return false;
+    }
+}
+
+function validarEquiposOctavos2() {
+    var combo = document.getElementById("equipo1_octavos2");
+    var equipo1 = combo.options[combo.selectedIndex].text;
+    var combo1 = document.getElementById("equipo2_octavos2");
+    var equipo2 = combo1.options[combo1.selectedIndex].text;
+
+    if (equipo1 == equipo2) {
+        alert("ERROR. No se puede enfrentar un mismo equipo entre si.");
+        return false;
+    }
+}
+
+function validarEquiposOctavos3() {
+    var combo = document.getElementById("equipo1_octavos3");
+    var equipo1 = combo.options[combo.selectedIndex].text;
+    var combo1 = document.getElementById("equipo2_octavos3");
+    var equipo2 = combo1.options[combo1.selectedIndex].text;
+
+    if (equipo1 == equipo2) {
+        alert("ERROR. No se puede enfrentar un mismo equipo entre si.");
+        return false;
+    }
+}
+
+function validarEquiposOctavos4() {
+    var combo = document.getElementById("equipo1_octavos4");
+    var equipo1 = combo.options[combo.selectedIndex].text;
+    var combo1 = document.getElementById("equipo2_octavos4");
+    var equipo2 = combo1.options[combo1.selectedIndex].text;
+
+    if (equipo1 == equipo2) {
+        alert("ERROR. No se puede enfrentar un mismo equipo entre si.");
+        return false;
+    }
+}
+
+function validarEquiposOctavos5() {
+    var combo = document.getElementById("equipo1_octavos5");
+    var equipo1 = combo.options[combo.selectedIndex].text;
+    var combo1 = document.getElementById("equipo2_octavos5");
+    var equipo2 = combo1.options[combo1.selectedIndex].text;
+
+    if (equipo1 == equipo2) {
+        alert("ERROR. No se puede enfrentar un mismo equipo entre si.");
+        return false;
+    }
+}
+
+function validarEquiposOctavos6() {
+    var combo = document.getElementById("equipo1_octavos6");
+    var equipo1 = combo.options[combo.selectedIndex].text;
+    var combo1 = document.getElementById("equipo2_octavos6");
+    var equipo2 = combo1.options[combo1.selectedIndex].text;
+
+    if (equipo1 == equipo2) {
+        alert("ERROR. No se puede enfrentar un mismo equipo entre si.");
+        return false;
+    }
+}
+
+function validarEquiposOctavos7() {
+    var combo = document.getElementById("equipo1_octavos7");
+    var equipo1 = combo.options[combo.selectedIndex].text;
+    var combo1 = document.getElementById("equipo2_octavos7");
+    var equipo2 = combo1.options[combo1.selectedIndex].text;
+
+    if (equipo1 == equipo2) {
+        alert("ERROR. No se puede enfrentar un mismo equipo entre si.");
+        return false;
+    }
+}
+
+function validarEquiposOctavos8() {
+    var combo = document.getElementById("equipo1_octavos8");
+    var equipo1 = combo.options[combo.selectedIndex].text;
+    var combo1 = document.getElementById("equipo2_octavos8");
+    var equipo2 = combo1.options[combo1.selectedIndex].text;
+
+    if (equipo1 == equipo2) {
+        alert("ERROR. No se puede enfrentar un mismo equipo entre si.");
+        return false;
+    }
+}
+
+
+
+function validarEquiposCuartos1() {
+    var combo = document.getElementById("equipo1_cuartos1");
+    var equipo1 = combo.options[combo.selectedIndex].text;
+    var combo1 = document.getElementById("equipo2_cuartos1");
+    var equipo2 = combo1.options[combo1.selectedIndex].text;
+
+    if (equipo1 == equipo2) {
+        alert("ERROR. No se puede enfrentar un mismo equipo entre si.");
+        return false;
+    }
+}
+
+function validarEquiposCuartos2() {
+    var combo = document.getElementById("equipo1_cuartos2");
+    var equipo1 = combo.options[combo.selectedIndex].text;
+    var combo1 = document.getElementById("equipo2_cuartos2");
+    var equipo2 = combo1.options[combo1.selectedIndex].text;
+
+    if (equipo1 == equipo2) {
+        alert("ERROR. No se puede enfrentar un mismo equipo entre si.");
+        return false;
+    }
+}
+
+function validarEquiposCuartos3() {
+    var combo = document.getElementById("equipo1_cuartos3");
+    var equipo1 = combo.options[combo.selectedIndex].text;
+    var combo1 = document.getElementById("equipo2_cuartos3");
+    var equipo2 = combo1.options[combo1.selectedIndex].text;
+
+    if (equipo1 == equipo2) {
+        alert("ERROR. No se puede enfrentar un mismo equipo entre si.");
+        return false;
+    }
+}
+
+function validarEquiposCuartos4() {
+    var combo = document.getElementById("equipo1_cuartos4");
+    var equipo1 = combo.options[combo.selectedIndex].text;
+    var combo1 = document.getElementById("equipo2_cuartos4");
+    var equipo2 = combo1.options[combo1.selectedIndex].text;
+
+    if (equipo1 == equipo2) {
+        alert("ERROR. No se puede enfrentar un mismo equipo entre si.");
+        return false;
+    }
+}
+
+function validarEquiposSemis1() {
+    var combo = document.getElementById("equipo1_semis1");
+    var equipo1 = combo.options[combo.selectedIndex].text;
+    var combo1 = document.getElementById("equipo2_semis1");
+    var equipo2 = combo1.options[combo1.selectedIndex].text;
+
+    if (equipo1 == equipo2) {
+        alert("ERROR. No se puede enfrentar un mismo equipo entre si.");
+        return false;
+    }
+}
+
+function validarEquiposSemis2() {
+    var combo = document.getElementById("equipo1_semis2");
+    var equipo1 = combo.options[combo.selectedIndex].text;
+    var combo1 = document.getElementById("equipo2_semis2");
+    var equipo2 = combo1.options[combo1.selectedIndex].text;
+
+    if (equipo1 == equipo2) {
+        alert("ERROR. No se puede enfrentar un mismo equipo entre si.");
+        return false;
+    }
+}
+
+function validarEquiposFinal() {
+    var combo = document.getElementById("equipo1_final");
+    var equipo1 = combo.options[combo.selectedIndex].text;
+    var combo1 = document.getElementById("equipo2_final");
+    var equipo2 = combo1.options[combo1.selectedIndex].text;
+
+    if (equipo1 == equipo2) {
+        alert("ERROR. No se puede enfrentar un mismo equipo entre si.");
+        return false;
+    }
+}
